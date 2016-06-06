564c9ddd519f367ecb2f8e4a3291118702a58356
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-06-06 08:01:29 +0200

    Fix test errors

--- test/livereload-loader.spec.js
+++ test/livereload-loader.spec.js
@@ -1,20 +1,24 @@
-var loader = require('../lib/livereload-loader');
+var loader = require('../lib/livereload-loader'),
+    path = require('path');
 
-exports.replace = function(test) {
+exports.replace = function (test) {
     // given
+
+    var dirname = path.resolve(__dirname, '..', 'node_modules', 'tvml-kit-livereload');
     var given = `App.onLaunch = function () {
     }`,
-        expected =  `var liveReload = require('tvml-kit-livereload/lib/livereload');
+        expected = `var liveReload = require('` + dirname + `/lib/livereload');
 
 App.onLaunch = function () {
   liveReload.connect('http://localhost:9000', App);
     }
 `;
-    this.cacheable=function(){};
-    this.query =" 9000";
+    this.cacheable = function () {
+    };
+    this.query = " 9000";
     // when
-    var result = loader.apply(this,[given]);
+    var result = loader.apply(this, [given]);
     // then
-    test.ok(result.localeCompare(expected)===0, 'Should replace source');
+    test.ok(result.localeCompare(expected) === 0, 'Should replace source');
     test.done();
-}
+};
